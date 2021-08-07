97ba4ed9063b16cdf4a6db4e7e5dc5c13def3565
Vitaly Raevsky <vitaly.raevsky@gmail.com>
2021-08-07 03:26:29 +0300

    add working with AES keys less android M

--- app/src/main/java/com/otus/myapplication/crypto/Keys.kt
+++ app/src/main/java/com/otus/myapplication/crypto/Keys.kt
@@ -2,22 +2,34 @@ package com.otus.myapplication.crypto
 
 import android.content.Context
 import android.os.Build
+import android.security.KeyPairGeneratorSpec
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import android.util.Base64
 import androidx.annotation.RequiresApi
 import androidx.security.crypto.MasterKey
+import java.math.BigInteger
 import java.security.KeyPair
 import java.security.KeyPairGenerator
 import java.security.KeyStore
 import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.SecureRandom
+import java.util.Calendar
+import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
+import javax.security.auth.x500.X500Principal
 
 private const val KEY_PROVIDER = "AndroidKeyStore"
 private const val KEY_LENGTH = 256
 
 private const val RSA_ALGORITHM = "RSA"
 private const val RSA_KEY_ALIAS = "RSA_DEMO"
+private const val RSA_MODE_LESS_THAN_M = "RSA/ECB/PKCS1Padding"
+private const val SHARED_PREFERENCE_NAME = "RSAEncryptedKeysSharedPreferences"
+private const val ENCRYPTED_KEY_NAME = "RSAEncryptedKeysKeyName"
 
 private const val AES_ALGORITHM = "AES"
 private const val AES_KEY_ALIAS = "AES_DEMO"
@@ -26,26 +38,72 @@ class Keys(
     private val applicationContext: Context
 ) {
 
+    private val sharedPreferences by lazy {
+        applicationContext.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
+    }
+
     private val keyStore by lazy {
         KeyStore.getInstance(KEY_PROVIDER).apply {
             load(null)
         }
     }
 
+    fun removeKeys(keyAlias: String) {
+        keyStore.deleteEntry(keyAlias);
+    }
+
     fun getAesSecretKey(): SecretKey {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             keyStore.getKey(AES_KEY_ALIAS, null) as? SecretKey ?: generateAesSecretKey()
         } else {
-
+            getAesSecretKeyLessThanM() ?: generateAesSecretKey()
         }
+    }
+
+    private fun getAesSecretKeyLessThanM(): SecretKey? {
+        val encryptedKeyBase64Encoded = getSecretKeyFromSharedPreferences()
+        return encryptedKeyBase64Encoded?.let {
+            val encryptedKey = Base64.decode(encryptedKeyBase64Encoded, Base64.DEFAULT)
+            val key = rsaDecryptKey(encryptedKey)
+            SecretKeySpec(key, "AES")
+        }
+    }
+
+    private fun rsaDecryptKey(encryptedKey: ByteArray?): ByteArray {
+        val cipher = Cipher.getInstance(RSA_MODE_LESS_THAN_M)
+        cipher.init(Cipher.DECRYPT_MODE, getRsaPrivateKey())
+        return cipher.doFinal(encryptedKey)
+    }
+
+    private fun getSecretKeyFromSharedPreferences(): String? {
+        return sharedPreferences.getString(ENCRYPTED_KEY_NAME, null)
     }
 
     private fun generateAesSecretKey(): SecretKey {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getKeyGenerator().generateKey()
         } else {
-
+            generateAndSaveAesSecretKeyLessThanM()
         }
+    }
+
+    private fun generateAndSaveAesSecretKeyLessThanM(): SecretKey {
+        val key = ByteArray(16)
+        SecureRandom().run {
+            nextBytes(key)
+        }
+        val encryptedKeyBase64encoded = Base64.encodeToString(rsaEncryptKey(key), Base64.DEFAULT)
+        sharedPreferences.edit().apply {
+            putString(ENCRYPTED_KEY_NAME, encryptedKeyBase64encoded)
+            apply()
+        }
+        return SecretKeySpec(key, "AES")
+    }
+
+    private fun rsaEncryptKey(secret: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance(RSA_MODE_LESS_THAN_M)
+        cipher.init(Cipher.ENCRYPT_MODE, getRsaPublicKey())
+        return cipher.doFinal(secret)
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -66,27 +124,47 @@ class Keys(
             .build()
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
     fun getRsaKeyPair(): KeyPair {
         val privateKey = keyStore.getKey(RSA_KEY_ALIAS, null) as? PrivateKey
         val publicKey = keyStore.getCertificate(RSA_KEY_ALIAS).publicKey
         return privateKey?.let { KeyPair(publicKey, privateKey) } ?: generateRsaSecretKey()
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
+    private fun getRsaPrivateKey(): PrivateKey {
+        return keyStore.getKey(RSA_KEY_ALIAS, null) as? PrivateKey ?: generateRsaSecretKey().private
+    }
+
+    private fun getRsaPublicKey(): PublicKey {
+        return keyStore.getCertificate(RSA_KEY_ALIAS).publicKey as? PublicKey ?: generateRsaSecretKey().public
+    }
+
     private fun generateRsaSecretKey(): KeyPair {
-        val spec = KeyGenParameterSpec.Builder(
-            RSA_KEY_ALIAS,
-            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
-        )
-            .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
-            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
-            .setUserAuthenticationRequired(true)
-            .setRandomizedEncryptionRequired(false)
-            .build()
-        val kpg = KeyPairGenerator.getInstance(RSA_ALGORITHM, KEY_PROVIDER)
-        kpg.initialize(spec)
-        return kpg.generateKeyPair()
+        val spec = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            KeyGenParameterSpec.Builder(
+                RSA_KEY_ALIAS,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            )
+                .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+                .setUserAuthenticationRequired(true)
+                .setRandomizedEncryptionRequired(false)
+                .build()
+        } else {
+            val start: Calendar = Calendar.getInstance()
+            val end: Calendar = Calendar.getInstance()
+            end.add(Calendar.YEAR, 30)
+            KeyPairGeneratorSpec.Builder(applicationContext)
+                .setAlias(RSA_KEY_ALIAS)
+                .setSubject(X500Principal("CN=$RSA_KEY_ALIAS"))
+                .setSerialNumber(BigInteger.TEN)
+                .setStartDate(start.getTime())
+                .setEndDate(end.getTime())
+                .build()
+        }
+        return KeyPairGenerator.getInstance(RSA_ALGORITHM, KEY_PROVIDER).run {
+            initialize(spec)
+            generateKeyPair()
+        }
     }
 
     fun getMasterKey(keyScheme: MasterKey.KeyScheme): MasterKey {
